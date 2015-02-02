32de04a2a58ed87c72952df789b0f68c4e717c76
Brian Wigginton <brianwigginton@gmail.com>
2015-02-02 13:07:52 -0600

    adds some goodies to brew file

--- brew.sh
+++ brew.sh
@@ -8,8 +8,6 @@ echo -e "\nInstalling Formulas"
 brew install caskroom/cask/brew-cask
 
 brew tap homebrew/versions
-brew tap caskroom/versions
-brew tap caskroom/fonts
 
 # Formulas
 
@@ -26,27 +24,46 @@ brew install watch
 brew install wget
 
 # Casks
+
+brew tap caskroom/versions
+brew tap caskroom/unofficial
+brew tap caskroom/fonts
+
 # global apps
 brew cask install --appdir="/Applications" dropbox
 
 # user apps
 #brew cask install boxer
+brew cask install appcleaner
+brew cask install base
+brew cask install caffeine
 brew cask install cakebrew
 brew cask install chromecast
 brew cask install charles
 brew cask install color-oracle
+brew cask install dash
+brew cask install fantastical
 brew cask install firefox
-brew cask install google-chrome
+brew cask install google-chrome --appdir="/Applications"
 brew cask install google-hangouts # hangouts support plugins
+brew cask install gpgtools
+brew cask install istat-menus
 brew cask install iterm2
 brew cask install eclipse-ide
 brew cask install macvim
+brew cask install mou
+brew cask install nvalt
 brew cask install opera
+brew cask install sequel-pro-nightly
 brew cask install sketchup
+brew cask install slack
 brew cask install sourcetree
+brew cask install sublime-text3
 brew cask install spotify
 brew cask install spotify-notifications
+brew cask install textwrangler
 brew cask install vagrant
+brew cask install xscope
 
 brew cleanup --force
 
