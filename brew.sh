461571dfeb28fef505b4686b5f6ec08f2fffd945
Brian Wigginton <brianwigginton@gmail.com>
2017-02-09 09:55:50 -0600

    updating brew.sh

--- brew.sh
+++ brew.sh
@@ -4,14 +4,11 @@ brew update;
 
 echo -e "\nInstalling Formulas"
 
-# install brew cask
-brew install caskroom/cask/brew-cask
-
 brew tap homebrew/versions
 brew tap homebrew/binary
 
 # for joe (gitignore tool) - http://karan.github.io/joe/
-brew tap karan/karan 
+brew tap karan/karan
 
 # Formulas
 
@@ -24,8 +21,9 @@ brew install jsdoc3
 brew install ncdu
 brew install nmap
 brew install pyenv
+brew install safe-rm
 brew install socat
-brew install the_silver_searcher 
+brew install the_silver_searcher
 brew install vim
 brew install watch
 brew install wget
@@ -36,44 +34,38 @@ brew tap caskroom/versions
 brew tap caskroom/unofficial
 brew tap caskroom/fonts
 
-# global apps
-brew cask install --appdir="/Applications" dropbox
-
 # user apps
-#brew cask install boxer
-brew cask install appcleaner
+brew cask install 1password
+brew cask install bartender
 brew cask install base
 brew cask install caffeine
 brew cask install cakebrew
 brew cask install chromecast
 brew cask install charles
-brew cask install color-oracle
+#brew cask install color-oracle
 brew cask install dash
+brew cask install dropbox
 brew cask install dnscrypt
-brew cask install fantastical
 brew cask install firefox
 brew cask install genymotion
 brew cask install google-chrome --appdir="/Applications"
 brew cask install google-hangouts # hangouts support plugins
-brew cask install gpgtools
 brew cask install hex-fiend
+brew cask install hyperdock
 brew cask install istat-menus
 brew cask install iterm2
-brew cask install eclipse-ide
-brew cask install macvim
-brew cask install mou
-brew cask install nvalt
-brew cask install opera
 brew cask install sequel-pro-nightly
-brew cask install sketchup
 brew cask install slack
 brew cask install sourcetree
-brew cask install sublime-text3
 brew cask install spotify
-brew cask install spotify-notifications
-brew cask install textwrangler
+brew cask install virtualbox
+#brew cask install textwrangler
 brew cask install vagrant
-brew cask install xscope
+brew cask install visual-studio-code
+brew cask install wmail
+#brew cask install xscope
+
+brew cask install font-droid-sans-mono-for-powerline
 
 brew cleanup --force
 
